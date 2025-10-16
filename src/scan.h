2fa4c1640a4918516a4aa3332bfb16dc7e7d4f64
Tobias Rausch <rauschtobi@gmail.com>
2025-10-16 11:45:22 +0200

    parse modifications

--- src/scan.h
+++ src/scan.h
@@ -20,8 +20,16 @@
 namespace cameo
 {
 
+  struct ModHit {
+    int32_t pos;
+    char code;
+    uint8_t prob;
+
+    ModHit(int32_t const p, char const c, uint8_t const r) : pos(p), code(c), prob(r) {}
+  };
+
   template<typename TConfig>
-  inline void
+  inline bool
   findPromoters(TConfig const& c) {
     // Open file handles
     typedef std::vector<samFile*> TSamFile;
@@ -35,6 +43,16 @@ namespace cameo
     }
     bam_hdr_t* hdr = sam_hdr_read(samfile[0]);
 
+    // Load FASTA
+    char* seq = NULL;
+    faidx_t* fai = fai_load(c.genome.string().c_str());
+    if (fai == NULL) {
+      if (fai_build(c.genome.string().c_str()) == -1) {
+	std::cerr << "Fail to open genome fai index for " << c.genome.string() << std::endl;
+	return false;
+      } else fai = fai_load(c.genome.string().c_str());
+    }
+    
     // Keep track of avg. coverage
     typedef boost::accumulators::accumulator_set<double, boost::accumulators::features<boost::accumulators::tag::mean, boost::accumulators::tag::variance> > TAccumulator;
     std::vector<TAccumulator> acc(c.files.size());
@@ -45,13 +63,23 @@ namespace cameo
     
     // Iterate chromosomes
     for(int32_t refIndex=0; refIndex < (int32_t) hdr->n_targets; ++refIndex) {
+      // Load chromosome
+      std::string tname = hdr->target_name[refIndex];
+      int32_t seqlen = -1;
+      seq = faidx_fetch_seq(fai, tname.c_str(), 0, hdr->target_len[refIndex], &seqlen);
+      
       
       // Collect reads from all samples
       for(unsigned int file_c = 0; file_c < c.files.size(); ++file_c) {
 
 	typedef uint16_t TCovValue;
 	TCovValue maxval = std::numeric_limits<TCovValue>::max();
-	std::vector<TCovValue> cov(hdr->target_len[refIndex], 0);
+	std::vector<TCovValue> cov_plus(hdr->target_len[refIndex], 0);
+	std::vector<TCovValue> cov_minus(hdr->target_len[refIndex], 0);
+	std::vector<TCovValue> m_plus(hdr->target_len[refIndex], 0);
+	std::vector<TCovValue> m_minus(hdr->target_len[refIndex], 0);
+	std::vector<TCovValue> h_plus(hdr->target_len[refIndex], 0);
+	std::vector<TCovValue> h_minus(hdr->target_len[refIndex], 0);
 	
 	// Read alignments
 	hts_itr_t* iter = sam_itr_queryi(idx[file_c], refIndex, 0, hdr->target_len[refIndex]);
@@ -64,7 +92,8 @@ namespace cameo
 	  
 	  std::size_t seed = hash_lr(rec);
 	  //std::cerr << bam_get_qname(rec) << '\t' << seed << std::endl;
-	  
+
+	  // Parse quality and seq
 	  typedef std::vector<uint8_t> TQuality;
 	  TQuality quality;
 	  quality.resize(rec->core.l_qseq);
@@ -76,24 +105,109 @@ namespace cameo
 	    quality[i] = qualptr[i];
 	    sequence[i] = "=ACMGRSVTWYHKDBN"[bam_seqi(seqptr, i)];
 	  }
+
+	  // Parse MM and ML tags (if present)
+	  std::vector<ModHit> modhits;
+	  uint8_t* mm_aux = bam_aux_get(rec, "MM");
+	  if (mm_aux && *mm_aux == 'Z') {
+	    const char* mmstr = (const char*)(mm_aux + 1);
+	    std::string s(mmstr);
+	    std::vector<std::string> tokens;
+	    boost::split(tokens, s, boost::is_any_of(";"));
+	    for(const auto& tok : tokens) {
+	      if (tok.size() == 0) continue;
+	      // parse <base><strand><modcode>,<pos>,<pos>,...
+	      std::size_t idx_tok = 0;
+	      char base = tok[idx_tok++];
+	      if (idx_tok >= tok.size()) continue;
+	      char strand = tok[idx_tok++];
+	      std::string mod_codes;
+	      while ((idx_tok < tok.size()) && (tok[idx_tok] != ',')) mod_codes.push_back(tok[idx_tok++]);
+	      if ((idx_tok < tok.size()) && (tok[idx_tok] == ',')) {
+		std::string pos_str = tok.substr(idx_tok+1);
+		if (!pos_str.empty()) {
+		  std::vector<std::string> pos_tokens;
+		  boost::split(pos_tokens, pos_str, boost::is_any_of(","));
+		  int32_t current = -1;
+		  for(const auto& ptoken : pos_tokens) {
+		    if (ptoken.size() == 0) continue;
+		    int32_t delta = 0;
+		    delta = std::stoi(ptoken);
+		    current += (delta + 1);
+		    for(char mc : mod_codes) modhits.push_back(ModHit(current, mc, 255));
+		  }
+		}
+	      }
+	    }
+	  }
+
+	  // Parse ML tag
+	  uint8_t* ml_aux = bam_aux_get(rec, "ML");
+	  if (ml_aux && *ml_aux == 'B') {
+	    char subtype = *(char*)(ml_aux + 1);
+	    if (subtype == 'C') {
+	      const uint8_t* p = ml_aux + 2;
+	      int32_t n = 0;
+	      n = p[0] | (p[1]<<8) | (p[2]<<16) | (p[3]<<24);
+	      const uint8_t* data = p + 4;
+	      int32_t assign = std::min<int32_t>(n, (int32_t) modhits.size());
+	      for(int32_t i = 0; i < assign; ++i) modhits[i].prob = data[i];
+	    }
+	  }
+
+	  // Build map from read position to modification hits
+	  std::unordered_map<int32_t, std::vector<ModHit> > modByPos;
+	  for(const auto& mh : modhits) modByPos[mh.pos].push_back(mh);
 	  
 	  // Parse cigar
 	  uint32_t rp = rec->core.pos; // reference pointer
 	  uint32_t sp = 0; // sequence pointer
 	  uint32_t* cigar = bam_get_cigar(rec);
+	  bool readRev = (rec->core.flag & BAM_FREVERSE);
 	  for (std::size_t i = 0; i < rec->core.n_cigar; ++i) {
-	    if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
-	      for(std::size_t k = 0; k<bam_cigar_oplen(cigar[i]); ++k, ++rp, ++sp) {
-		if (cov[rp] < maxval) ++cov[rp];
+	    uint32_t op = bam_cigar_op(cigar[i]);
+	    uint32_t oplen = bam_cigar_oplen(cigar[i]);
+	    if ((op == BAM_CMATCH) || (op == BAM_CEQUAL) || (op == BAM_CDIFF)) {
+	      for(uint32_t k = 0; k < oplen; ++k, ++rp, ++sp) {
+		if (rp >= hdr->target_len[refIndex]) break;
+		// Coverage
+		if (readRev) {
+		  if (cov_minus[rp] < maxval) ++cov_minus[rp];
+		} else {
+		  if (cov_plus[rp] < maxval) ++cov_plus[rp];
+		}
+		// Mods
+		auto it = modByPos.find(sp);
+		if (it != modByPos.end()) {
+		  for(const auto& mh : it->second) {
+		    char mm_strand = '+';
+		    bool modOnRef = !readRev;
+		    if ((mh.code == 'm') || (mh.code == 'M')) {
+		      if (modOnRef) {
+			if (m_plus[rp] < maxval) ++m_plus[rp];
+		      } else {
+			if (m_minus[rp] < maxval) ++m_minus[rp];
+		      }
+		    } else if ((mh.code == 'h') || (mh.code == 'H')) {
+		      if (modOnRef) {
+			if (h_plus[rp] < maxval) ++h_plus[rp];
+		      } else {
+			if (h_minus[rp] < maxval) ++h_minus[rp];
+		      }
+		    } else {
+		      std::cerr << "Warning: Unknown modification code! " << mh.code << std::endl;
+		    }
+		  }
+		}
 	      }
-	    } else if (bam_cigar_op(cigar[i]) == BAM_CDEL) {
-	      rp += bam_cigar_oplen(cigar[i]);
-	    } else if (bam_cigar_op(cigar[i]) == BAM_CINS) {
-	      sp += bam_cigar_oplen(cigar[i]);
-	    } else if ((bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) || (bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP)) {
-	      sp += bam_cigar_oplen(cigar[i]);
-	    } else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) {
-	      rp += bam_cigar_oplen(cigar[i]);
+	    } else if (op == BAM_CDEL) {
+	      rp += oplen;
+	    } else if (op == BAM_CINS) {
+	      sp += oplen;
+	    } else if ((op == BAM_CSOFT_CLIP) || (op == BAM_CHARD_CLIP)) {
+	      sp += oplen;
+	    } else if (op == BAM_CREF_SKIP) {
+	      rp += oplen;
 	    } else {
 	      std::cerr << "Warning: Unknown Cigar operation!" << std::endl;
 	    }
@@ -103,19 +217,42 @@ namespace cameo
 	hts_itr_destroy(iter);
 	
 	// Summarize coverage for this chromosome
-	for(uint32_t i = 0; i < hdr->target_len[refIndex]; ++i) acc[file_c](cov[i]);
+	for(uint32_t i = 0; i < hdr->target_len[refIndex]; ++i) acc[file_c](cov_plus[i]);
 	double sdcov = sqrt(boost::accumulators::variance(acc[file_c]));
 	double avgcov = boost::accumulators::mean(acc[file_c]);
 	std::cerr << c.files[file_c].string() << ", " << hdr->target_name[refIndex] << ", meancov=" << avgcov << ", sdcov=" << sdcov << std::endl;
+
+	// Output percent modified per site
+	// Header: file,chrom,1-based-pos,refbase,cov_plus,cov_minus,mod_m_plus,mod_m_minus,mod_h_plus,mod_h_minus,percent_modified(+),percent_modified(-),percent_modified(collapsed)
+	bool onlyCpG = false;
+	for (uint32_t pos = 0; pos < hdr->target_len[refIndex]; ++pos) {
+	  // optionally count only CpG sites
+	  if (onlyCpG) {
+	    if (pos + 1 >= hdr->target_len[refIndex]) continue;
+	    char b1 = std::toupper(seq[pos]);
+	    char b2 = std::toupper(seq[pos+1]);
+	    if (!(b1 == 'C' && b2 == 'G')) continue;
+	  }
+	  if (h_minus[pos]) std::cerr << hdr->target_name[refIndex] << "\t" << (pos + 1) << "\t" << (pos + 2) << "\th\t" << cov_minus[pos] << "\t-\t" << (pos + 1) << "\t" << (pos + 2) << "\t255,0,0\t" << cov_minus[pos] << "\t" << ((cov_minus[pos] > 0) ? 100.0 * double(h_minus[pos]) / double(cov_minus[pos]) : 0.0) << "\t" << h_minus[pos] << std::endl;
+	  if (h_plus[pos]) std::cerr << hdr->target_name[refIndex] << "\t" << (pos + 1) << "\t" << (pos + 2) << "\th\t" << cov_plus[pos] << "\t-\t" << (pos + 1) << "\t" << (pos + 2) << "\t255,0,0\t" << cov_plus[pos] << "\t" << ((cov_plus[pos] > 0) ? 100.0 * double(h_plus[pos]) / double(cov_plus[pos]) : 0.0) << "\t" << h_plus[pos] << std::endl;
+	  if (m_minus[pos]) std::cerr << hdr->target_name[refIndex] << "\t" << (pos + 1) << "\t" << (pos + 2) << "\tm\t" << cov_minus[pos] << "\t-\t" << (pos + 1) << "\t" << (pos + 2) << "\t255,0,0\t" << cov_minus[pos] << "\t" << ((cov_minus[pos] > 0) ? 100.0 * double(m_minus[pos]) / double(cov_minus[pos]) : 0.0) << "\t" << m_minus[pos] << std::endl;
+	  if (m_plus[pos]) std::cerr << hdr->target_name[refIndex] << "\t" << (pos + 1) << "\t" << (pos + 2) << "\tm\t" << cov_plus[pos] << "\t-\t" << (pos + 1) << "\t" << (pos + 2) << "\t255,0,0\t" << cov_plus[pos] << "\t" << ((cov_plus[pos] > 0) ? 100.0 * double(m_plus[pos]) / double(cov_plus[pos]) : 0.0) << "\t" << m_plus[pos] << std::endl;
+	}
       }
+      // Free space
+      if (seq != NULL) free(seq);
+      exit(-1);
     }
 
     // Clean-up
+    fai_destroy(fai);
     bam_hdr_destroy(hdr);
     for(unsigned int file_c = 0; file_c < c.files.size(); ++file_c) {
       hts_idx_destroy(idx[file_c]);
       sam_close(samfile[file_c]);
     }
+
+    return true;
   }
   
 }
